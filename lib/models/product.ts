eb10dd9ea6e78cadc9b9b50d30ea3bf259303284
taaliikramesef-ui <taaliikramesef-ui@users.noreply.github.com>
2026-04-30 20:13:42 +0100

    product schema

---
+++ lib/models/product.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        ingredients: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        stock: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        images: {
+            type: [String],
+            validate: [
+                (val: string[]) => val.length <= 3,
+                '{PATH} exceeds the limit of 3 images'
+            ]
+        },
+        category: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export default mongoose.models.Product || mongoose.model("Product", ProductSchema);
