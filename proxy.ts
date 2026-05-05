f2c92d2d09f0deacd937de8f58f4ca8815e07713
taaliikramesef-ui <taaliikramesef-ui@users.noreply.github.com>
2026-05-05 19:49:33 +0100

    fix authentication improve admin security

--- proxy.ts
+++ proxy.ts
@@ -7,9 +7,11 @@ const intlMiddleware = createMiddleware(routing);
 export default auth((req) => {
     // Check if the current path starts with /admin (considering optional locale)
     const pathname = req.nextUrl.pathname;
+    const isAdmin = req.auth?.user !== undefined && "role" in req.auth.user && req.auth.user.role === "admin"
 
-    if (req.auth && startsWith(pathname, "/admin/login")) return Response.redirect(new URL("/admin", req.nextUrl))
+    if (isAdmin && startsWith(pathname, "/admin/login")) return Response.redirect(new URL("/admin", req.nextUrl))
     if (!req.auth && startsWith(pathname, "/admin") && !startsWith(pathname, "/admin/login")) return Response.redirect(new URL("/admin/login", req.nextUrl))
+    if (!isAdmin && req.auth && startsWith(pathname, "/admin")) return Response.redirect(new URL("/", req.nextUrl))
     if (req.auth && startsWith(pathname, "/manage-subscription")) return Response.redirect(new URL("/", req.nextUrl))
     if (!req.auth && !startsWith(pathname, "/manage-subscription") && !startsWith(pathname, "/admin/login")) return Response.redirect(new URL("/manage-subscription", req.nextUrl))
 
